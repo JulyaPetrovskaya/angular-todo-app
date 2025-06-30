d1641f1da85161c995fe2e8d4ed1b3826d1d5701
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2025-06-30 17:05:48 +0300

    remove todos was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Todo } from './types/todo';
 
-const todos = [
+const todosFromServer = [
   { id: 1, title: 'HTML + CSS', completed: true },
   { id: 2, title: 'JS', completed: false },
   { id: 3, title: 'React', completed: false },
@@ -13,17 +13,40 @@ const todos = [
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
+export class AppComponent implements OnInit {
+  _todos: Todo[] = [];
+  activeTodos: Todo[] = [];
+
+  get todos() {
+     // get — це геттер, тобто властивість,
+     // яку ми читаємо як змінну, а не викликаємо як функцію
+    return this._todos;
+  }
+
+  set todos(todos: Todo[]) {
+    if (todos === this._todos) {
+      return;
+    }
+    this._todos = todos;
   
-export class AppComponent {
-  todos = todos;
+    this.activeTodos = this._todos.filter((todo) => !todo.completed);
+    // отримаємо масив незавершених задач — фільтрує всі елементи
+    // масиву todos, залишаючи лише ті,
+    // в яких todo.completed === false
+  }
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    this.todos = todosFromServer;
+  }
+  
   todoForm = new FormGroup({
     title: new FormControl('', {
       nonNullable: true,
-      validators: [
-      Validators.required,
-      Validators.minLength(3),
-      ]
+      validators: [Validators.required, Validators.minLength(3)],
     }),
   });
 
@@ -31,29 +54,48 @@ export class AppComponent {
     return this.todoForm.get('title') as FormControl;
   }
 
-
-  get activeTodos() {
-    // get — це геттер, тобто властивість,
-    // яку ми читаємо як змінну, а не викликаємо як функцію
-
-    return this.todos.filter((todo) => !todo.completed);
-    // отримаємо масив незавершених задач — фільтрує всі елементи
-    // масиву todos, залишаючи лише ті,
-    // в яких todo.completed === false
-  }
-
-  addTodo() {
+  handleFormSubmit() {
     if (this.todoForm.invalid) {
       return;
     }
+
+    this.addTodo(this.title.value);
+    this.todoForm.reset();
+  }
+
+  addTodo(newTitle: string) {
     // якщо title не порожній, то створюємо новий об'єкт Todo
     const newTodo: Todo = {
       id: Date.now(),
       title: this.title.value as string,
       completed: false,
     };
-    // створюємо новий об'єкт Todo
-    this.todos.push(newTodo);
-    this.todoForm.reset();
+      // створюємо новий об'єкт Todo
+    this.todos = [
+      ...this.todos,
+      newTodo,
+    ];
+  }
+
+  renameTodo(todoId: number, title: string) {
+    this.todos = this.todos.map((todo) => {
+      if (todo.id !== todoId) {
+        return todo;
+      }
+      return {...todo, title};
+    });
+  }
+
+  toggleTodo(todoId: number) {
+    this.todos = this.todos.map((todo) => {
+      if (todo.id !== todoId) {
+        return todo;
+      }
+      return {...todo, completed: !todo.completed};
+    });
+  }
+
+  deleteTodo(todoId: number) {
+    this.todos = this.todos.filter((todo) => todo.id !== todoId);
   }
 }
